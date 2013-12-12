30666bd534db691b070d40569e1cd7751d81f001
Harjeet Singh <harjeetss@gmail.com>
2013-12-12 17:39:57 +0530

    Adding template for polls/index.html abd

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,22 @@
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from polls.models import Poll
 
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 
 def results(request, poll_id):
